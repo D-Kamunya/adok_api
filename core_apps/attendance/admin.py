ff85fe9841deb7c5450de81d413f7ce919d1c578
D-Kamunya <kinc.developer@gmail.com>
2025-06-11 13:28:48 +0300

    feat: add attendance models and register them

--- core_apps/attendance/admin.py
+++ core_apps/attendance/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import AttendanceRecord
 
-# Register your models here.
+@admin.register(AttendanceRecord)
+class AttendanceRecordAdmin(admin.ModelAdmin):
+    list_display = (
+        'congregation', 'sunday_date',
+        'sunday_school', 'adults', 'youth', 'diff_abled',
+        'total_collection', 'banked', 'unbanked'
+    )
+    list_filter = ('sunday_date', 'congregation__parish__archdeaconry')
+    search_fields = ('congregation__name', 'congregation__parish__name')
+    date_hierarchy = 'sunday_date'
--- core_apps/attendance/models.py
+++ core_apps/attendance/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from core_apps.common.models import TimeStampedModel,Archdeaconry,Parish,Congregation
+from core_apps.analyzer.models import UploadedWorkbook
 
-# Create your models here.
+class AttendanceRecord(TimeStampedModel):
+    """
+    Stores weekly attendance and collection data for a Congregation on a given Sunday.
+    """
+    workbook = models.ForeignKey(
+        UploadedWorkbook,
+        related_name='attendance_records',
+        on_delete=models.CASCADE
+    )
+    archdeaconry = models.ForeignKey(
+        Archdeaconry,
+        related_name='attendance_records',
+        on_delete=models.PROTECT
+    )
+    parish = models.ForeignKey(
+        Parish,
+        related_name='attendance_records',
+        on_delete=models.PROTECT
+    )
+    congregation = models.ForeignKey(
+        Congregation,
+        related_name='attendance_records',
+        on_delete=models.PROTECT
+    )
+    sunday_date = models.DateField()
+    sunday_school = models.PositiveIntegerField(default=0)
+    adults = models.PositiveIntegerField(default=0)
+    diff_abled = models.PositiveIntegerField(default=0, verbose_name="diff_abled")
+    youth = models.PositiveIntegerField(default=0)
+    total_collection = models.DecimalField(max_digits=12, decimal_places=2)
+    banked = models.DecimalField(max_digits=12, decimal_places=2)
+    unbanked = models.DecimalField(max_digits=12, decimal_places=2)
+    remarks = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ('congregation', 'sunday_date')
+        ordering = ['-sunday_date', 'archdeaconry', 'parish', 'congregation']
+        indexes = [
+            models.Index(fields=['sunday_date']),
+            models.Index(fields=['archdeaconry', 'parish']),
+        ]
+
+    def __str__(self):
+        return f"{self.congregation.name} @ {self.sunday_date}"
