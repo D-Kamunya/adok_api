720ea5ac66173174f4845c3a2a687edb9ccf17b4
D-Kamunya <kinc.developer@gmail.com>
2025-07-22 15:40:12 +0300

    chore: edit start django bat file to run silently

--- start_django.bat
+++ start_django.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "I:\coding\ACK\arch-api"
 call .venv\Scripts\activate
-start /min "" python manage.py runserver 0.0.0.0:8000 >> django_startup_log.txt 2>&1
-exit
+python manage.py runserver 0.0.0.0:8000 >> django_startup_log.txt 2>&1
+
