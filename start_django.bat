9104094aa6f6aefe3f013c299b4ab4d2e7417e03
D-Kamunya <kinc.developer@gmail.com>
2025-07-22 15:43:19 +0300

    ch: move bat startup logs in logs folder

--- start_django.bat
+++ start_django.bat
@@ -1,5 +1,9 @@
 @echo off
 cd /d "I:\coding\ACK\arch-api"
+:: Ensure logs directory exists
+if not exist logs (
+    mkdir logs
+)
 call .venv\Scripts\activate
-python manage.py runserver 0.0.0.0:8000 >> django_startup_log.txt 2>&1
+python manage.py runserver 0.0.0.0:8000 >> logs/django_startup_log.txt 2>&1
 
