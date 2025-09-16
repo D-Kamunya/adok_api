4677dc090b40a01bda84cf76afaa63ed36114528
D-Kamunya <kinc.developer@gmail.com>
2025-09-16 20:47:08 +0300

    chore: copy .envs during deployment

--- do_server_deploy.sh
+++ do_server_deploy.sh
@@ -22,6 +22,10 @@ git archive --format tar --output ./project.tar master
 echo "Uploading project to $DIGITAL_OCEAN_IP_ADDRESS..."
 rsync -avz ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
 
+echo "Uploading env files..."
+rsync -avz .envs/ root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/.envs/
+rsync -avz ./client/.env root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/.envs/client.env
+
 # 🚀 Connect to server and deploy
 ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS <<'ENDSSH'
 
@@ -32,12 +36,16 @@ cleanup_remote(){
   echo "Cleaning up remote temp files..."
   rm -rf /tmp/project.tar
   rm -rf /app
+  rm -rf /tmp/.envs
 }
 trap cleanup_remote EXIT
 
 TEMP_DIR=$(mktemp -d)
 echo "Extracting to $TEMP_DIR..."
 tar -xf /tmp/project.tar -C "$TEMP_DIR"
+
+rsync -av --exclude='client.env' /tmp/.envs/ "$TEMP_DIR/.envs/"
+rsync -av /tmp/.envs/client.env "$TEMP_DIR/client/.env"
 
 cd "$TEMP_DIR"
 
